31fa70a1b12bd1efd1f965935cd93fbfd9625298
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-01-14 14:52:10 +0100

    chore: Initial commit
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ bindings/go/binding_test.go
@@ -0,0 +1,22 @@
+package tree_sitter_markdown_test
+
+import (
+	"testing"
+
+	tree_sitter_markdown "github.com/ponyruntime/tree-sitter-markdown/bindings/go"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+)
+
+func TestCanLoadBlockGrammar(t *testing.T) {
+	language := tree_sitter.NewLanguage(tree_sitter_markdown.Language())
+	if language == nil {
+		t.Errorf("Error loading Markdown block grammar")
+	}
+}
+
+func TestCanLoadInlineGrammar(t *testing.T) {
+	language := tree_sitter.NewLanguage(tree_sitter_markdown.InlineLanguage())
+	if language == nil {
+		t.Errorf("Error loading Markdown inline grammar")
+	}
+}
