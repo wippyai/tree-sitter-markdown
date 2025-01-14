c03626f9b0d2d84bafb81c03468b60b2a7a07dd2
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-01-14 15:18:55 +0100

    chore: minor fixes
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 all install uninstall clean:
 	$(MAKE) -C tree-sitter-markdown $@
-	$(MAKE) -C tree-sitter-markdown-inline $@
 
 .PHONY: all install uninstall clean
--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -13,10 +13,3 @@ func TestCanLoadBlockGrammar(t *testing.T) {
 		t.Errorf("Error loading Markdown block grammar")
 	}
 }
-
-func TestCanLoadInlineGrammar(t *testing.T) {
-	language := tree_sitter.NewLanguage(tree_sitter_markdown.InlineLanguage())
-	if language == nil {
-		t.Errorf("Error loading Markdown inline grammar")
-	}
-}
